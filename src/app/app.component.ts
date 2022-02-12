1f9902692417990f23d9af6f74016d30cc84a5d5
Emre YİĞİT <emreygt67@icloud.com>
2022-02-12 21:45:31 +0300

    12.02.2022

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './app.component.html',//app.component.html nin çalışmasını sağlar
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'first-proje';
+  title = 'first-proje' ;
 }
